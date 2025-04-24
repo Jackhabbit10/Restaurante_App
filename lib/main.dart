169978a1fa041af9124b8baee94506743caa733e
Jackhabbit10 <mauricioikier@hotmail.com>
2025-04-24 20:55:07 -0300

    Criando a imagem de fundo

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,48 @@ class Restaurant extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
+      body: Container(
+        decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("Imagens/restaurant.png"),
+          fit: BoxFit.cover
+          )
+
+        ),
+        child:  Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
             "Pode entrar!",
             style: TextStyle(fontSize: 30, color: Colors.white),
           ),
-          Text("0", style: TextStyle(fontSize: 100, color: Colors.white)),
-          Row(children: [
-            TextButton(onPressed: null, child: Text("Entrou", style: TextStyle(fontSize: 30, color: Colors.white))),
-            TextButton(onPressed: null, child: Text("Saiu",   style: TextStyle(fontSize: 30, color: Colors.white)))
+          Text("0", style: TextStyle(fontSize: 100, color: const Color.fromARGB(255, 212, 51, 51))),
+          Row( mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: null,
+              child: Text("Entrou"), 
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.white,
+              fixedSize: Size(100, 100),
+              shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(24))
+              )
+              ),
+
+              SizedBox(width: 50,),
+
+            TextButton(onPressed: null, 
+            child: Text("Saiu"),
+             style: TextButton.styleFrom(
+              backgroundColor: Colors.white,
+              fixedSize: Size(100, 100),
+              shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(24))
+              )
+              )
           ],),
         ],
       ),
+      )
     );
   }
 }

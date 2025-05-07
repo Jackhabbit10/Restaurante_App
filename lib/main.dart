2fb25889e242a6f371a5fdfc2c924fcd23f793bf
Jackhabbit10 <mauricioikier@hotmail.com>
2025-05-06 22:54:20 -0300

    Dando função aos botões

--- lib/main.dart
+++ lib/main.dart
@@ -4,55 +4,89 @@ void main() {
   runApp(MaterialApp(home: Restaurant()));
 }
 
-class Restaurant extends StatelessWidget {
+class Restaurant extends StatefulWidget {
   const Restaurant({super.key});
 
   @override
+  State<Restaurant> createState() => _RestaurantState();
+}
+
+class _RestaurantState extends State<Restaurant> {
+  @override
+  int contador = 0;
+  String mensagem = "Pode entrar!";
+  void incremento() {
+    if (contador != 10) {
+      setState(() {
+        contador++;
+      });
+    }
+  }
+
+  void decremento() {
+    if (contador != 0) {
+      setState(() {
+        contador--;
+      });
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Container(
         decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("Imagens/restaurant.png"),
-          fit: BoxFit.cover
-          )
-
-        ),
-        child:  Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Pode entrar!",
-            style: TextStyle(fontSize: 30, color: Colors.white),
+          image: DecorationImage(
+            image: AssetImage("Imagens/restaurant.png"),
+            fit: BoxFit.cover,
           ),
-          Text("0", style: TextStyle(fontSize: 100, color: const Color.fromARGB(255, 212, 51, 51))),
-          Row( mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextButton(
-              onPressed: null,
-              child: Text("Entrou"), 
-            style: TextButton.styleFrom(
-              backgroundColor: Colors.white,
-              fixedSize: Size(100, 100),
-              shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(24))
-              )
+            Text(
+              contador == 10 ? "Lotado!" : "Pode entrar!",
+              style: TextStyle(fontSize: 30, color: Colors.white),
+            ),
+            Text(
+              contador.toString(),
+              style: TextStyle(
+                fontSize: 100,
+                color: const Color.fromARGB(255, 212, 51, 51),
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: incremento,
+                  child: Text("Entrou"),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    fixedSize: Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                ),
 
-              SizedBox(width: 50,),
-
-            TextButton(onPressed: null, 
-            child: Text("Saiu"),
-             style: TextButton.styleFrom(
-              backgroundColor: Colors.white,
-              fixedSize: Size(100, 100),
-              shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(24))
-              )
-              )
-          ],),
-        ],
+                SizedBox(width: 50),
+                TextButton(
+                  onPressed: decremento,
+                  child: Text("Saiu"),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    fixedSize: Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      )
     );
   }
 }
